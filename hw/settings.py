bcf94b939b26b10b974e087e79b186d3a97f53b6
Eddie Welker <eddie.welker@gmail.com>
2012-09-08 00:17:23 -0400

    in working demo state

--- hw/settings.py
+++ hw/settings.py
@@ -56,17 +56,17 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/site_media'
+MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = here('site_media')
+STATIC_ROOT = here('../site_media/static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/site_media/'
+STATIC_URL = '/site_media/static'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
